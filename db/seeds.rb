8eb32545f8ff7da58140ea561923b9df2d80bc3a
Joel Plotnik <joelplotnik@gmail.com>
2022-08-30 10:50:31 -0700

    Build message resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "robin", password: "password")
-User.create(username: "poppy", password: "password")
-User.create(username: "wylder", password: "password")
-User.create(username: "aero", password: "password")
-User.create(username: "pete", password: "password")
+Message.create(body: "Hello everyone", user: User.find(1))
+Message.create(body: "Hey new guy", user: User.find(2))
+Message.create(body: "I'm chillin' like a villain", user: User.find(3))
+Message.create(body: "Hi I'm a new message", user: User.find(4))
+Message.create(body: "Hi I'm a new message", user: User.find(5))
